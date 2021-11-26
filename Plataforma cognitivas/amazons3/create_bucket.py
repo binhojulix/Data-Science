ee452e6b3491b3851ef159e42e0ef57c90182493
fabiojdluz <fabio.jdluz@gmail.com>
2021-11-26 09:13:29 -0300

    commit

--- Plataforma cognitivas/amazons3/create_bucket.py
+++ Plataforma cognitivas/amazons3/create_bucket.py
@@ -10,4 +10,20 @@ def creating_bucket(bucket_name):
         print('Done!')
     except ClientError as e:
         logging.error(e)
+        print('Bucket não criado!')
+
+
+def creating_bucket_with_region(bucket_name, region):
+     try:
+         s3 = boto3.client('s3', region_name=region)       
+         location = {'LocationConstraint': region}
+         s3.create_bucket(Bucket=bucket_name,
+                          CreateBucketConfiguration=location)
+    except ClientError as e:
+        logging.error(e)
         print('Bucket not created!')
+
+        response = s3.list_buckets()
+        print('Existing buckets:')
+        for bucket in response['Buckets']:
+            print(f'{bucket['Name']}')
